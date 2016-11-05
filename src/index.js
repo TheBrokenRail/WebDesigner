003e792a01b0b9ef306776630b90d06aa81a3211
TheBrokenRail <connor24nolan@live.com>
2016-11-04 20:56:54 -0400

    Add Code For Button

--- src/index.js
+++ src/index.js
@@ -17,6 +17,19 @@ function add(type) {
                 document.getElementById("options").innerHTML = "";
                 site.push(obj);
             }
+        case 1:
+            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Action (JS): </p><input id=\"js\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
+            var add = document.getElementById("add");
+            add.onclick = function() {
+                var text = document.getElementById("text");
+                var js = document.getElementById("js");
+                var obj = new WebObject();
+                obj.type = 1;
+                obj.data[0] = text.value;
+                obj.data[1] = js.value;
+                document.getElementById("options").innerHTML = "";
+                site.push(obj);
+            }
     }
     var exit = document.getElementById("exit");
     exit.onclick = function() {
