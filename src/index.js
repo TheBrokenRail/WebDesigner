9601c9c62c3a5e9ea642a5b4c12cbc1c3575703d
TheBrokenRail <connor24nolan@live.com>
2016-11-04 20:50:30 -0400

    Add Text

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 function WebObject() {
-    this.type = "";
+    this.type = null;
     this.data = [];
 }
 function add(type) {
+    var options = document.getElementById("options");
     switch(type) {
         case 0:
-            console.log("text");
+            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
+            var add = document.getElementById("add");
+            add.onclick = function() {
+                var text = document.getElementById("text");
+                var obj = new WebObject();
+                obj.type = 0;
+                obj.data[0] = text.value;
+                document.getElementById("options").innerHTML = "";
+            }
+    }
+    var exit = document.getElementById("exit");
+    exit.onclick = function() {
+        document.getElementById("options").innerHTML = "";
     }
 }
 var site = [];
