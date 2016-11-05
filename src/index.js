11bfc2d42ef11ca0bc1d98e399852fb3136d2e0d
TheBrokenRail <connor24nolan@live.com>
2016-11-04 22:01:55 -0400

    Add Image

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ function edit(num) {
         case 0:
             options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button>";
             var save = document.getElementById("save");
+            var text = document.getElementById("text");
+            text.value = site[num].data[0];
             save.onclick = function() {
-                var text = document.getElementById("text");
                 site[num].data[0] = text.value;
                 document.getElementById("options").innerHTML = "";
                 render();
@@ -18,11 +19,24 @@ function edit(num) {
         case 1:
             options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Action (JS): </p><input id=\"js\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button>";
             var save = document.getElementById("save");
+            var text = document.getElementById("text");
+            text.value = site[num].data[0];
+            var js = document.getElementById("js");
+            js.value = site[num].data[1];
             save.onclick = function() {
-                var text = document.getElementById("text");
-                var js = document.getElementById("js");
                 site[num].data[0] = text.value;
                 site[num].data[1] = js.value;
+                document.getElementById("options").innerHTML = "";
+                render();
+            }
+            break;
+        case 0:
+            options.innerHTML = "<p>Source: </p><input id=\"text\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button>";
+            var save = document.getElementById("save");
+            var text = document.getElementById("text");
+            text.value = site[num].data[0];
+            save.onclick = function() {
+                site[num].data[0] = text.value;
                 document.getElementById("options").innerHTML = "";
                 render();
             }
@@ -49,6 +63,12 @@ function render() {
                 button.innerHTML = site[i].data[0];
                 eval("button.onclick = function() {edit(" + i + ")};");
                 preview.appendChild(button);
+                break;
+            case 3:
+                var img = document.createElement("IMG");
+                img.src = site[i].data[0];
+                eval("img.onclick = function() {edit(" + i + ")};");
+                preview.appendChild(img);
                 break;
         }
     }
@@ -79,6 +99,19 @@ function add(type) {
                 obj.type = 1;
                 obj.data[0] = text.value;
                 obj.data[1] = js.value;
+                document.getElementById("options").innerHTML = "";
+                site.push(obj);
+                render();
+            }
+            break;
+        case 2:
+            options.innerHTML = "<p>Source: </p><input id=\"text\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
+            var add = document.getElementById("add");
+            add.onclick = function() {
+                var text = document.getElementById("text");
+                var obj = new WebObject();
+                obj.type = 2;
+                obj.data[0] = text.value;
                 document.getElementById("options").innerHTML = "";
                 site.push(obj);
                 render();
