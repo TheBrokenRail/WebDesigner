6bbc7fe7bbf732260a81caaff1d59703b39560c4
TheBrokenRail <connor24nolan@live.com>
2016-11-06 08:11:14 -0500

    Add CSS

--- src/index.js
+++ src/index.js
@@ -6,65 +6,80 @@ function WebObject() {
 function edit(num) {
     switch(site[num].type) {
         case 0:
-            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
+            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
             var save = document.getElementById("save");
             var text = document.getElementById("text");
             text.value = site[num].data[0];
+            var css = document.getElementById("css");
+            css.value = site[num].data[1];
             save.onclick = function() {
                 site[num].data[0] = text.value;
+                site[num].data[1] = css.value;
                 document.getElementById("options").innerHTML = "";
                 render(false);
             }
             break;
         case 1:
-            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Action (JS): </p><input id=\"js\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
+            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Action (JS): </p><input id=\"js\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
             var save = document.getElementById("save");
             var text = document.getElementById("text");
             text.value = site[num].data[0];
             var js = document.getElementById("js");
             js.value = site[num].data[1];
+            var css = document.getElementById("css");
+            css.value = site[num].data[2];
             save.onclick = function() {
                 site[num].data[0] = text.value;
                 site[num].data[1] = js.value;
+                site[num].data[2] = css.value;
                 document.getElementById("options").innerHTML = "";
                 render(false);
             }
             break;
         case 2:
-            options.innerHTML = "<p>Source: </p><input id=\"text\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
+            options.innerHTML = "<p>Source: </p><input id=\"text\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
             var save = document.getElementById("save");
             var text = document.getElementById("text");
             text.value = site[num].data[0];
+            var css = document.getElementById("css");
+            css.value = site[num].data[1];
             save.onclick = function() {
                 site[num].data[0] = text.value;
+                site[num].data[1] = css.value;
                 document.getElementById("options").innerHTML = "";
                 render(false);
             }
             break;
         case 3:
-            options.innerHTML = "<p>Default Text: </p><input id=\"text\"></input><br><p>ID: </p><input id=\"id\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
+            options.innerHTML = "<p>Default Text: </p><input id=\"text\"></input><br><p>ID: </p><input id=\"id\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
             var save = document.getElementById("save");
             var text = document.getElementById("text");
             text.value = site[num].data[0];
             var id = document.getElementById("id");
             id.value = site[num].data[1];
+            var css = document.getElementById("css");
+            css.value = site[num].data[2];
             save.onclick = function() {
                 site[num].data[0] = text.value;
                 site[num].data[1] = id.value;
+                site[num].data[2] = css.value;
                 document.getElementById("options").innerHTML = "";
                 render(false);
             }
             break;
         case 4:
-            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Link: </p><input id=\"link\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
+            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Link: </p><input id=\"link\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"save\">Save</button><button id=\"exit\">Exit</button><button id=\"delete\">Delete</button><button id=\"up\">Up</button><button id=\"down\">Down</button>";
             var save = document.getElementById("save");
             var text = document.getElementById("text");
             text.value = site[num].data[0];
             var link = document.getElementById("link");
             link.value = site[num].data[1];
+            var css = document.getElementById("css");
+            css.value = site[num].data[2];
             save.onclick = function() {
                 site[num].data[0] = text.value;
                 site[num].data[1] = link.value;
+                site[num].data[2] = css.value;
                 document.getElementById("options").innerHTML = "";
                 render(false);
             }
@@ -109,6 +124,7 @@ function render(real) {
                 if (!real) {
                     text.setAttribute("onclick","edit(" + i + ");");
                 }
+                link.setAttribute("style",site[i].data[1]);
                 preview.appendChild(text);
                 break;
             case 1:
@@ -119,6 +135,7 @@ function render(real) {
                 } else {
                     button.setAttribute("onclick",site[i].data[1]);
                 }
+                link.setAttribute("style",site[i].data[2]);
                 preview.appendChild(button);
                 break;
             case 2:
@@ -127,6 +144,7 @@ function render(real) {
                 if (!real) {
                     img.setAttribute("onclick","edit(" + i + ");");
                 }
+                link.setAttribute("style",site[i].data[1]);
                 preview.appendChild(img);
                 break;
             case 3:
@@ -136,6 +154,7 @@ function render(real) {
                 if (!real) {
                     input.setAttribute("onclick","edit(" + i + ");");
                 }
+                link.setAttribute("style",site[i].data[2]);
                 preview.appendChild(input);
                 break;
             case 4:
@@ -147,6 +166,7 @@ function render(real) {
                 } else {
                     link.setAttribute("href",site[i].data[1]);
                 }
+                link.setAttribute("style",site[i].data[2]);
                 preview.appendChild(link);
                 break;
         }
@@ -156,71 +176,81 @@ function add(type) {
     var options = document.getElementById("options");
     switch(type) {
         case 0:
-            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
+            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
             var add = document.getElementById("add");
             add.onclick = function() {
                 var text = document.getElementById("text");
+                var css = document.getElementById("css");
                 var obj = new WebObject();
                 obj.type = 0;
                 obj.data[0] = text.value;
+                obj.data[1] = css.value;
                 document.getElementById("options").innerHTML = "";
                 site.push(obj);
                 render(false);
             }
             break;
         case 1:
-            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Action (JS): </p><input id=\"js\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
+            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Action (JS): </p><input id=\"js\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
             var add = document.getElementById("add");
             add.onclick = function() {
                 var text = document.getElementById("text");
                 var js = document.getElementById("js");
+                var css = document.getElementById("css");
                 var obj = new WebObject();
                 obj.type = 1;
                 obj.data[0] = text.value;
                 obj.data[1] = js.value;
+                obj.data[2] = css.value;
                 document.getElementById("options").innerHTML = "";
                 site.push(obj);
                 render(false);
             }
             break;
         case 2:
-            options.innerHTML = "<p>Source: </p><input id=\"text\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
+            options.innerHTML = "<p>Source: </p><input id=\"text\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
             var add = document.getElementById("add");
             add.onclick = function() {
                 var text = document.getElementById("text");
+                var css = document.getElementById("css");
                 var obj = new WebObject();
                 obj.type = 2;
                 obj.data[0] = text.value;
+                obj.data[1] = css.value;
                 document.getElementById("options").innerHTML = "";
                 site.push(obj);
                 render(false);
             }
             break;
         case 3:
-            options.innerHTML = "<p>Default Text: </p><input id=\"text\"></input><br><p>ID: </p><input id=\"id\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
+            options.innerHTML = "<p>Default Text: </p><input id=\"text\"></input><br><p>ID: </p><input id=\"id\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
             var add = document.getElementById("add");
             add.onclick = function() {
                 var text = document.getElementById("text");
                 var id = document.getElementById("id");
+                var css = document.getElementById("css");
                 var obj = new WebObject();
                 obj.type = 3;
                 obj.data[0] = text.value;
                 obj.data[1] = id.value;
+                obj.data[2] = css.value;
                 document.getElementById("options").innerHTML = "";
                 site.push(obj);
                 render(false);
             }
             break;
         case 4:
-            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Link: </p><input id=\"link\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
+            options.innerHTML = "<p>Text: </p><input id=\"text\"></input><br><p>Link: </p><input id=\"link\"></input><br><p>CSS: </p><input id=\"caa\"></input><br><button id=\"add\">Add</button><button id=\"exit\">Exit</button>";
             var add = document.getElementById("add");
             add.onclick = function() {
                 var text = document.getElementById("text");
                 var link = document.getElementById("link");
+                var css = document.getElementById("css");
                 var obj = new WebObject();
                 obj.type = 4;
                 obj.data[0] = text.value;
                 obj.data[1] = link.value;
+                obj.data[2] = css.value;
                 document.getElementById("options").innerHTML = "";
                 site.push(obj);
                 render(false);
